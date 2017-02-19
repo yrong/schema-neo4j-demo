3057f1ee89f89d71456a0200ec7b10dfee152047
yrong <yrong1997@gmail.com>
2017-02-19 21:22:31 +0800

    封装脚本重建ITService和Group之间的从属关系(2h)
    封装脚本重建ITService和Group之间的从属关系

--- script/init.sh
+++ script/init.sh
@@ -4,7 +4,7 @@ curl -XDELETE 'http://localhost:9200/cmdb/'
 
 curl --header "Content-Type: application/json" -XPUT 'http://localhost:9200/cmdb/' -d @./search/schema.json
 
-$NEO4J_HOME/bin/neo4j-shell -file ./cypher/initSchema.cyp
+/bin/bash ./script/execute_cypher.sh ./cypher/initSchema.cyp
 
 
 
