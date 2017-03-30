e97bb2ee343d28e187af191ce7fdbc941246bdfc
yrong <yrong1997@gmail.com>
2017-03-30 11:57:15 +0800

    按版本类型区分调整构建，缺省发布类型不包含搜索，导入导出，备份恢复等增值应用(3h)
    按版本类型区分调整构建，缺省发布类型不包含搜索，导入导出，备份恢复等增值应用
    babel相关组件升级
    dropSchema添加user.alias，和init保持一致

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ var webpack = require('webpack');
 var path = require('path');
 var fs = require("file-system");
 
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-var CleanWebpackPlugin = require('clean-webpack-plugin');
-var WebpackShellPlugin = require('webpack-shell-plugin');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 var mods = {};
 fs.readdirSync("node_modules")
@@ -14,28 +14,35 @@ fs.readdirSync("node_modules")
         mods[mod] = "commonjs " + mod;
     });
 
+var devtool = 'source-map'
+
+var entry = {server:'./server.js'}
+var packages = [
+    {from:'config',to:'config'},{from:'cypher/initSchema.cyp',to:'cypher/initSchema.cyp'},
+    {from:'public',to:'public'},{from:'cypher/QueryMountedCabinet.cyp',to:'cypher/QueryMountedCabinet.cyp'},
+    {from:'test/*.json'},{from:'node_modules',to:'node_modules'},
+    {from:'search',to:'search',ignore:['*.js']}
+]
+if(process.env.EDITION === 'advanced'){
+    entry = Object.assign(entry,{exportJSON:'./export/json/index.js',importExcel:'./import/excel/index.js',importJSON:'./import/json/index.js'})
+    packages = [...packages,{from:'script',to:'script'}]
+}else{
+    packages = [...packages,{from:'script/init.sh',to:'script/init.sh'},{from:'script/execute_cypher.sh',to:'script/execute_cypher.sh'},{from:'script/jq-linux64',to:'script/jq-linux64'}]
+}
+
 var plugins = [
-    new UglifyJSPlugin({
-        compress: {
-            warnings: false
-        },
-        output: {
-            comments: false
-        }
+    new webpack.optimize.UglifyJsPlugin({
+        sourceMap: devtool && (devtool.indexOf("sourcemap") >= 0 || devtool.indexOf("source-map") >= 0)
     }),
-    new CopyWebpackPlugin([
-        {from:'config',to:'config'},{from:'cypher/*.cyp'},{from:'public',to:'public'},
-        {from:'script',to:'script'},{from:'test/*.json'},{from:'node_modules',to:'node_modules'},
-        {from:'search',to:'search',ignore:['*.js']}
-    ], {ignore: ['*.gitignore']}),
+    new CopyWebpackPlugin(packages, {ignore: ['*.gitignore']}),
     new CleanWebpackPlugin(['build']),
     new WebpackShellPlugin({onBuildStart:['echo "Webpack Start"'], onBuildEnd:['/bin/bash ./postbuild.sh']})
 ];
 
 var config = {
     target: 'node',
-    entry: {server:'./server.js',exportJSON:'./export/json/index.js',importExcel:'./import/excel/index.js',importJSON:'./import/json/index.js'},
-    // devtool: 'source-map',
+    entry: entry,
+    devtool: devtool,
     output: {
         path: path.join(__dirname, 'build'),
         filename: '[name].js'
@@ -51,10 +58,7 @@ var config = {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
                 use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['es2015']
-                    }
+                    loader: 'babel-loader'
                 }
             }
         ]
