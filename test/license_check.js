495d2a6e4ea2ccaea377f1bb122b3c5d4e87bc84
yrong <yrong1997@gmail.com>
2017-04-30 09:13:45 +0800

    expiration check from ntp server

--- test/license_check.js
+++ test/license_check.js
@@ -1,7 +1,12 @@
 // process.env.encryption_algorithm = 'aes'
 var path = require("path");
 var fs = require("fs");
-var licenseCheck = require('cmdb-license-checker')
+var licenseCheck = require('../wheels/license_check')
+var moment = require('moment')
 
-let license = licenseCheck.load(path.resolve('./CMDB-API.lic'))
-console.log(license)
+licenseCheck.load(path.resolve('./CMDB-API.lic'))
+console.log(licenseCheck.getLicense())
+setInterval(()=>{
+        console.log("public:"+licenseCheck.now().unix()),
+        console.log("local:"+ moment().unix())
+}, 1000)
