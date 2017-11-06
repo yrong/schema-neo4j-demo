e978c7210ed1feeca9dc9c8d731dee507aed0133
yrong <yrong1997@gmail.com>
2017-11-06 18:12:54 +0800

    update script

--- script/prepare.sh
+++ script/prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-testdir="$(dirname "$(pwd)")/test"
-if [ ! -d "${testdir}" ]; then
-  git clone https://git.coding.net/alien11/test.git
+if [ ! -d "config" ]; then
+  cd .. && git clone https://git.coding.net/alien11/test.git
+  cd CMDB-API
   ln -s ../test/config .
 fi
