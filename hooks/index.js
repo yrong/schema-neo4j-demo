7899493916473c90bbf3a38a4eab02ab404c9656
yrong <yrong1997@gmail.com>
2017-11-13 12:09:00 +0800

    配合config通过环境变量加载运行期文件及日志变更&cypher文件查找变更&依赖升级(1h)

--- hooks/index.js
+++ hooks/index.js
@@ -77,7 +77,7 @@ const generateQR = async (params,ctx)=>{
     for(let key in properties){
         if(params.fields[key]&&properties[key].generateQRImage){
             qr_code = qr.image(params.fields[key],{ type: 'png' })
-            qr_image_dir = config.get('runtime_data.base_dir')+config.get('runtime_data.cmdb.qr_image_dir')
+            qr_image_dir = (process.env['RUNTIME_PATH']||'../runtime') + config.get('runtime_data.cmdb.qr_image_dir')
             mkdirp.sync(qr_image_dir)
             qr_code.pipe(fs.createWriteStream(path.join(qr_image_dir,params.fields[key]+'.png')))
         }
@@ -120,7 +120,7 @@ module.exports = {
     },
     customizedQueryItems_preProcess:(params,ctx)=>{
         if(params.cypherQueryFile){
-            params.cypher = fs.readFileSync('cypher/'+params.cypherQueryFile + '.cyp', 'utf8')
+            params.cypher = fs.readFileSync(path.resolve(__dirname,'../cypher/'+params.cypherQueryFile + '.cyp'), "utf8")
         }
         requestHandler.logCypher(params)
         return params
