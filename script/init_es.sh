9c4b213bf115903fd35093fa166948b0a1be7e88
yrong <yrong1997@gmail.com>
2017-11-19 22:38:53 +0800

    cmdb容器化改造-2(4.5h)
    1,原neo4j,es的schema初始化通过数据库自带工具执行，容器环境不具备条件，改造为通过程序接口init.js执行(容器环境初始化方式参见npm
    run initSchemaInDocker)
    ２，json schema增加uniqueKeys描述唯一约束字段，初始化时创建neo4j相关唯一约束
    3,配合docker容器化改造，构建自身代码调整及简化重构（需先npm run
    linkWebpackConfig链接config相关配置,另外构建和运行之前需执行npm run setenv设置环境变量)

--- script/init_es.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-echo $'delete index'
-curl -XDELETE 'http://localhost:9200/cmdb,processflow/'
-echo $'\n\ncreate cmdb index and add schema in es'
-curl --header "Content-Type: application/json" -XPUT 'http://localhost:9200/cmdb/' -d @./search/cmdb.json
-echo $'\n\ncreate processflow index and add schema in es'
-curl --header "Content-Type: application/json" -XPUT 'http://localhost:9200/processflow/' -d @./search/processflow.json
--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,18 @@
-var path = require('path');
-var fs = require("fs");
+const webpack_config = require('./webpack')
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const WebpackShellPlugin = require('webpack-shell-plugin');
-const GitRevisionPlugin = require('git-revision-webpack-plugin')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-
-var mods = {};
-fs.readdirSync("node_modules")
-    .filter(x => [".bin"].indexOf(x) === -1)
-    .forEach(mod => {
-        mods[mod] = "commonjs " + mod;
-    });
-
-var devtool = 'source-map'
-
-var entry = {server:'./app.js'}
-var packages = [
+let entry = {server:'./app.js',init:'./init.js'}
+const packages = [
     {from:'cypher',to:'cypher',ignore:['*.js']},
     {from:'node_modules',to:'node_modules'},
+    {from:'search',to:'search',ignore:['*.js']},
+    {from:'schema',to:'schema',ignore:['*.js']}
 ]
+
 if(!process.env.EDITION){
-    packages = [...packages,{from:'script',to:'script'},{from:'search',to:'search',ignore:['*.js']},
-        {from:'schema',to:'schema',ignore:['*.js']},{from:'test/*.json'}]
-    entry = Object.assign(entry,{init:'./init.js',exportJSON:'./export/json/index.js',importJSON:'./import/json/index.js'})
+    entry = Object.assign(entry,{exportJSON:'./export/json/index.js',importJSON:'./import/json/index.js'})
 }
+module.exports = webpack_config(entry,packages)
 
-var releaseDir = process.env.ReleaseDir||path.join(__dirname, 'release')
-var edition = process.env.EDITION || 'essential'
 
-var plugins = [
-    new UglifyJSPlugin({
-        sourceMap: devtool && (devtool.indexOf("sourcemap") >= 0 || devtool.indexOf("source-map") >= 0)
-    }),
-    new CopyWebpackPlugin(packages, {ignore: ['*.gitignore']}),
-    new CleanWebpackPlugin(['build']),
-    new GitRevisionPlugin(),
-    new WebpackShellPlugin({onBuildStart:['echo "Build Start"'], onBuildEnd:[`/bin/bash ./postbuild.sh --dir=${releaseDir} --edition=${edition}`]})
-];
 
-var config = {
-    target: 'node',
-    entry: entry,
-    devtool: devtool,
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: '[name].js'
-    },
-    node: {
-        __filename: true,
-        __dirname: true
-    },
-    externals: mods,
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: plugins
-};
 
-module.exports = config;
