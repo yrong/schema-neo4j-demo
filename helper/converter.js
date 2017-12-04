b4fe241d70892aec15dfe8a3bbc50734bf2e1103
ronyang <ronyang@ronyangdeMacBook-Pro.local>
2017-12-05 03:41:57 +0800

    cache依赖升级&name到id转换移到网关侧(0.5h)

--- helper/converter.js
+++ helper/converter.js
@@ -9,12 +9,12 @@ const single_converter = async (key,value)=>{
     if(uuid_validator(value)||common.isLegacyUserId(key,value)){
         cached_val = await scirichon_cache.getItemByCategoryAndID(key,value)
     }else{
-        cached_val = await scirichon_cache.getItemByCategoryAndUniqueName(key,value)
+        throw new ScirichonError(`category ${key} as ${value} is not in uuid format`)
     }
     if(cached_val&&cached_val.uuid)
         uuid = cached_val.uuid
     else
-        throw new ScirichonError(`can not find category '${key}' with name or id as '${value}' in cmdb`)
+        throw new ScirichonError(`can not find category ${key} as ${value} in scirichon cache`)
     return uuid
 }
 const array_converter = async (key,values)=>{
