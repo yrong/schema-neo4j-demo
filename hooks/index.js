d44f391e06b4dcc2a7adf52603b0a8c1dd9f8160
yrong <yrong1997@gmail.com>
2018-01-04 13:40:23 +0800

    公共变更及重构合入cmdb&依赖升级&测试(2.5h)

--- hooks/index.js
+++ hooks/index.js
@@ -14,93 +14,7 @@ const requestHandler = require('./requestHandler')
 const responseHandler = require('./responseHandler')
 const logger = require('log4js_wrapper').getLogger()
 const cypherInvoker = require('../helper/cypherInvoker')
-
-const needNotify = (params,ctx)=>{
-    if(ctx.deleteAll || ctx.headers[common.TokenName] === common.InternalTokenId)
-        return false
-    if(params.batchImport)
-        return false
-    let schema_obj = schema.getAncestorSchema(params.category)
-    if(schema_obj&&schema_obj.notification)
-        return true
-}
-
-const stringify2Object = (params) => {
-    let objectFields=schema.getSchemaObjectProperties(params.category)
-    for(let key of objectFields){
-        if(_.isString(params[key])){
-            try{
-                params[key] = JSON.parse(params[key])
-            }catch(error){
-                //same field with different type in different categories(e.g:'status in 'ConfigurationItem' and 'ProcessFlow'),ignore error and just for protection here
-            }
-        }
-    }
-    return params
-}
-
-const addNotification = async (params,ctx)=>{
-    if(needNotify(params,ctx)){
-        let notification_obj = {type:params.category,user:ctx[common.TokenUserName],source:process.env['NODE_NAME']}
-        if(ctx.method === 'POST'){
-            notification_obj.action = 'CREATE'
-            notification_obj.new = stringify2Object(params.fields)
-        }
-        else if(ctx.method === 'PUT' || ctx.method === 'PATCH'){
-            notification_obj.action = 'UPDATE'
-            notification_obj.new = stringify2Object(params.fields)
-            notification_obj.old = stringify2Object(params.fields_old)
-            notification_obj.update = params.change
-        }else if(ctx.method === 'DELETE'){
-            notification_obj.action = 'DELETE'
-            notification_obj.old = stringify2Object(params.fields_old)
-        }
-        if(params.data&&params.data.notification){
-            if(params.data.notification.subscribe_user||params.data.notification.subscribe_role){
-                notification_obj.subscribe_user = params.data.notification.subscribe_user||[ctx[common.TokenUserName].uuid]
-                notification_obj.subscribe_role = params.data.notification.subscribe_role||ctx[common.TokenUserName].roles
-            }
-            if(params.data.notification.additional){
-                notification_obj.additional = params.data.notification.additional
-            }
-        }
-        await common.apiInvoker('POST',`http://${config.get('privateIP')||'localhost'}:${config.get('notifier.port')}`,'/api/notifications','',notification_obj)
-    }
-}
-
-const updateCache = async (params,ctx)=>{
-    if (ctx.method === 'DELETE'&&ctx.deleteAll) {
-        await scirichon_cache.flushAll()
-        return
-    }
-    if (ctx.method === 'POST' || ctx.method === 'PUT' || ctx.method === 'PATCH') {
-        await scirichon_cache.addItem(params.fields)
-    }
-    if (ctx.method === 'DELETE') {
-        await scirichon_cache.delItem(params.fields_old)
-    }
-}
-
-const updateSearch = async (params,ctx)=>{
-    if(ctx.method==='POST'||ctx.method==='PUT'||ctx.method==='PATCH'){
-        await search.addOrUpdateItem(params,ctx)
-    }
-    if(ctx.method==='DELETE'){
-        await search.deleteItem(params,ctx)
-    }
-}
-
-const generateQR = async (params,ctx)=>{
-    let properties=schema.getSchemaProperties(params.category),qr_code,qr_image_dir
-    for(let key in properties){
-        if(params.fields[key]&&properties[key].generateQRImage){
-            qr_code = qr.image(params.fields[key],{ type: 'png' })
-            qr_image_dir = (process.env['RUNTIME_PATH']||'../runtime') + config.get('runtime_data.cmdb.qr_image_dir')
-            mkdirp.sync(qr_image_dir)
-            qr_code.pipe(fs.createWriteStream(path.join(qr_image_dir,params.fields[key]+'.png')))
-        }
-    }
-}
+const requestPostHandler = require('./requestPostHandler')
 
 module.exports = {
     cudItem_preProcess: async function (params, ctx) {
@@ -109,7 +23,7 @@ module.exports = {
     },
     cudItem_postProcess:async function (result,params,ctx) {
         if(ctx.method==='POST'||ctx.method==='PUT'||ctx.method==='PATCH'){
-            await Promise.all([updateCache(params,ctx),updateSearch(params,ctx),addNotification(params,ctx),generateQR(params,ctx)]).catch((e)=>{
+            await Promise.all([requestPostHandler.updateCache(params,ctx),requestPostHandler.updateSearch(params,ctx),requestPostHandler.addNotification(params,ctx),requestPostHandler.generateQR(params,ctx)]).catch((e)=>{
                 logger.error(e.stack || e)
                 throw new ScirichonWarning(String(e))
             })
@@ -118,7 +32,7 @@ module.exports = {
             if(!ctx.deleteAll&&(!result||(result.length!=1))){
                 throw new ScirichonWarning('no record found')
             }
-            await Promise.all([updateCache(params,ctx),updateSearch(params,ctx),addNotification(params,ctx)]).catch((e)=>{
+            await Promise.all([requestPostHandler.updateCache(params,ctx),requestPostHandler.updateSearch(params,ctx),requestPostHandler.addNotification(params,ctx)]).catch((e)=>{
                 logger.error(e.stack || e)
                 throw new ScirichonWarning(String(e))
             })
--- search/index.js
+++ search/index.js
@@ -12,78 +12,75 @@ const requestHandler = require('../hooks/requestHandler')
 const responseHandler = require('../hooks/responseHandler')
 const hidden_fields = requestHandler.internalUsedFields
 
-const addOrUpdateItem = function(params, ctx) {
-    let schemas = schema.getSchemas(),typeName = schema.getAncestorCategory(params.category),indexName,index_obj,promise = Promise.resolve(params)
-    if(schemas[typeName]&&schemas[typeName].search){
-        indexName = schemas[typeName].search.index
+const addOrUpdateItem = async function(params, ctx) {
+    let schemas = schema.getSchemas(),category = schema.getAncestorCategory(params.category),index,index_obj
+    if(schemas[category]&&schemas[category].search){
+        index = schemas[category].search.index
         index_obj = {
-            index: indexName,
-            type: typeName,
-            id: params.uuid,
+            index: index,
+            type: category,
             refresh:true
         }
-        if(!ctx||ctx.method === 'POST'){
+        if(!schemas[category].search.upsert){
+            index_obj.id = params.uuid
+        }
+        if(!ctx||ctx.method === 'POST'||schemas[category].search.upsert){
             index_obj.body = _.omit(params,hidden_fields)
-            promise = es_client.index(index_obj)
+            await es_client.index(index_obj)
         }else if(ctx.method === 'PUT'||ctx.method === 'PATCH') {
             index_obj.body = {doc: _.omit(params, hidden_fields)}
-            promise = es_client.update(index_obj)
+            await es_client.update(index_obj)
         }
-        promise.then(()=>{
-            logger.debug(`add index in es:${JSON.stringify(index_obj,null,'\t')}`)
-        })
+        logger.debug(`add index in es:${JSON.stringify(index_obj,null,'\t')}`)
     }
-    return promise
 }
 
-const deleteItem = function(params, ctx) {
-    var queryObj = params.uuid?{term:{uuid:params.uuid}}:{match_all:{}}
-    let schemas = schema.getSchemas(),typeName = requestHandler.getCategoryFromUrl(ctx),indexName,promise = Promise.resolve(params)
-    if((schemas[typeName]&&schemas[typeName].search)||ctx.deleteAll){
-        if(ctx.deleteAll)
-            indexName = '*'
-        else
-            indexName = schemas[typeName].search.index
-        var delObj = {
-            index: indexName,
+const deleteItem = async function(params, ctx) {
+    let queryObj = params.uuid?{term:{uuid:params.uuid}}:{match_all:{}}
+    let schemas = schema.getSchemas(),category = requestHandler.getCategoryFromUrl(ctx),index
+    if(ctx.deleteAll){
+        index = '*'
+    }else if((schemas[category]&&schemas[category].search)){
+        index = schemas[category].search.index
+    }
+    if(index){
+        let delObj = {
+            index: index,
             body: {
                 query: queryObj
             },
             refresh:true
         }
-        promise = es_client.deleteByQuery(delObj).then(function () {
-            logger.debug(`delete index in es:${JSON.stringify(delObj,null,'\t')}`)
-        })
+        await es_client.deleteByQuery(delObj)
+        logger.debug(`delete index in es:${JSON.stringify(delObj,null,'\t')}`)
     }
-    return promise
 }
 
-const searchItem = (params, ctx)=> {
-    var query = params.uuid?`uuid:${params.uuid}`:(params.keyword?params.keyword:'*');
-    var _source = params._source?params._source.split(','):true;
-    var params_pagination = {"from":0,"size":config.get('perPageSize')},from;
+const searchItem = async (params, ctx)=> {
+    let query = params.uuid?`uuid:${params.uuid}`:(params.keyword?params.keyword:'*');
+    let _source = params._source?params._source.split(','):true;
+    let params_pagination = {"from":0,"size":config.get('perPageSize')},from;
     if(params.page&&params.per_page){
         from = (String)((parseInt(params.page)-1) * parseInt(params.per_page));
         params_pagination = {"from":from,"size":params.per_page}
     }
-    var queryObj = params.body?{body:params.body}:{q:query}
+    let queryObj = params.body?{body:params.body}:{q:query}
     let schemas = schema.getSchemas()
-    let typeName = params.category = params.category || requestHandler.getCategoryFromUrl(ctx)
-    let indexName = schemas[typeName].search.index
+    let category = params.category = params.category || requestHandler.getCategoryFromUrl(ctx)
+    let index = schemas[category].search.index
     if(queryObj.body&&queryObj.body.aggs){
         params_pagination = {size:0}
         params.aggs = true
     }
-    var searchObj = _.assign({
-        index: indexName,
-        type: params.category,
+    let searchObj = _.assign({
+        index: index,
+        type: category,
         _source:_source
     },queryObj,params_pagination)
     logger.debug(`search in es:${JSON.stringify(searchObj,null,'\t')}`)
-    return es_client.search(searchObj).then(async function (response) {
-        response = await responseHandler.esResponseMapper(response,params,ctx)
-        return response
-    });
+    let response = await es_client.search(searchObj)
+    response = await responseHandler.esResponseMapper(response,params,ctx)
+    return response
 }
 
 const checkStatus = ()=> {
@@ -99,7 +96,7 @@ const batchUpdate = async (category,uuids,body)=>{
             bulks.push({update:{_index: schemas[category].search.index, _type: category, _id: uuid}})
             bulks.push(_.omit(body, hidden_fields))
         }
-        await es_client.bulk({body:bulks})
+        await es_client.bulk({body:bulks,refresh:true})
     }
 }
 
